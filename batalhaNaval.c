765af64636200bb1f834205ac62bebee41fb690a
Oixt <millerrafael16@gmail.com>
2025-04-11 22:57:20 +0000

    batalha

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -7,19 +7,24 @@ int main() {
     int tabuleiro[10][10]={
         { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
         { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
-        { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
-        { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
-        { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+        { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 3 , 0 },
+        { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 3 , 0 },
+        { 0 , 3 , 3 , 3 , 0 , 0 , 0 , 0 , 3 , 0 },
         { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
         { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
         { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
         { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
         { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 }
     };
-    printf("*BATALHA NAVAL*");
-    printf("%c", letras);
+    printf("*BATALHA NAVAL*\n\n");
+    printf("  %c %c %c %c %c %c %c %c %c %c \n", letras[0],letras[1],letras[2],letras[3],letras[4],letras[5],letras[6],letras[7],letras[8],letras[9]);
 
-
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d",i+1);
+        printf(" %d %d %d %d %d %d %d %d %d %d  \n",tabuleiro[i][0], tabuleiro[i][1],tabuleiro[i][2],tabuleiro[i][3],tabuleiro[i][4],tabuleiro[i][5],tabuleiro[i][6],tabuleiro[i][7],tabuleiro[i][8],tabuleiro[i][9]);
+    }
+    
 
     return 0;
 }
